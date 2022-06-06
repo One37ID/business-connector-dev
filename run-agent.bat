e7991974c8ed6132f73cfc43efa6f1a9602d5778
Gary de Beer <dibbs.za@gmail.com>
2022-06-06 16:03:27 +0200

    updated

---
+++ run-agent.bat
@@ -0,0 +1,7 @@
+set DOCKER_PASS=
+set DOCKER_USER=
+set DOCKER_REGISTRY=registry.gitlab.com
+
+docker login -u %DOCKER_USER% -p %DOCKER_PASS% %DOCKER_REGISTRY%
+docker-compose -f docker-compose.yml up -d
+ngrok http 3000
